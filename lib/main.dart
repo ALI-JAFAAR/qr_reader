f521644db1e6f5317db0a76a37c7a9b438af85a4
Al-Mawadda-Apps <almawadda.app@gmail.com>
2023-08-26 11:03:44 +0300

    new update

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ void main() async {
         ChangeNotifierProvider.value(
           value: AppProvider(),
         ),
-
       ],
       child: const MyApp(),
     ),
--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -30,135 +30,137 @@ class _BodyWidgetState extends State<BodyWidget> {
           title: const Text('قارئ الـ QR'),
           centerTitle: true,
         ),
-        body: Center(
-          child: Column(
-            children: [
-              Container(
-                margin: const EdgeInsets.only(top: 8),
-                child: Image.asset(
-                  'assets/images/logo.png',
-                  height: 150,
-                  width: 160,
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(top: 8),
+                  child: Image.asset(
+                    'assets/images/logo.png',
+                    height: 150,
+                    width: 160,
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.9,
-                child: DropdownButtonHideUnderline(
-                  child: Container(
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black26),
-                        borderRadius: BorderRadius.circular(4)),
-                    child: DropdownButton<String>(
-                      borderRadius: BorderRadius.circular(20),
-                      value: city,
-                      onChanged: (newValue) {
-                        setState(() {
-                          city = newValue!;
-                        });
-                      },
-                      items: <String>[
-                        'اختر المدينة',
-                        ' امل ١ ',
-                        'امل ٢',
-                        'امل ٣'
-                      ].map<DropdownMenuItem<String>>((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 16),
-                            child: Text(
-                              value,
-                              style: const TextStyle(fontSize: 16),
+                const SizedBox(
+                  height: 30,
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  child: DropdownButtonHideUnderline(
+                    child: Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black26),
+                          borderRadius: BorderRadius.circular(4)),
+                      child: DropdownButton<String>(
+                        borderRadius: BorderRadius.circular(20),
+                        value: city,
+                        onChanged: (newValue) {
+                          setState(() {
+                            city = newValue!;
+                          });
+                        },
+                        items: <String>[
+                          'اختر المدينة',
+                          ' امل ١ ',
+                          'امل ٢',
+                          'امل ٣'
+                        ].map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 16),
+                              child: Text(
+                                value,
+                                style: const TextStyle(fontSize: 16),
+                              ),
                             ),
-                          ),
-                        );
-                      }).toList(),
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.9,
-                child: DropdownButtonHideUnderline(
-                  child: Container(
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black26),
-                        borderRadius: BorderRadius.circular(4)),
-                    child: DropdownButton<String>(
-                      borderRadius: BorderRadius.circular(20),
-                      value: gate,
-                      onChanged: (newValue) {
-                        setState(() {
-                          gate = newValue!;
-                        });
-                      },
-                      items: <String>[
-                        'اختر البوابة',
-                        'البوابة الاولى',
-                        'البوابة الثانية'
-                      ].map<DropdownMenuItem<String>>((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 16),
-                            child: Text(
-                              value,
-                              style: const TextStyle(fontSize: 16),
-                            ),
-                          ),
-                        );
-                      }).toList(),
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              SizedBox(
-                width: 150,
-                height: 50,
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.resolveWith(
-                      (states) => kPrimarycolor,
-                    ),
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) =>  QRScanner(city: city,gate: gate,),
+                          );
+                        }).toList(),
                       ),
-                    );
-                  },
-                  child: const Text(
-                    'قراءة الـ QR',
-                    style: TextStyle(fontSize: 18),
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              widget.status == 1
-                  ? Image.asset(
-                      'assets/images/true.png',
-                      width: 140,
-                    )
-                  : widget.status == 2
-                      ? Image.asset(
-                          'assets/images/false.png',
-                          width: 140,
-                        )
-                      : const Text('')
-            ],
+                const SizedBox(
+                  height: 15,
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  child: DropdownButtonHideUnderline(
+                    child: Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black26),
+                          borderRadius: BorderRadius.circular(4)),
+                      child: DropdownButton<String>(
+                        borderRadius: BorderRadius.circular(20),
+                        value: gate,
+                        onChanged: (newValue) {
+                          setState(() {
+                            gate = newValue!;
+                          });
+                        },
+                        items: <String>[
+                          'اختر البوابة',
+                          'البوابة الاولى',
+                          'البوابة الثانية'
+                        ].map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 16),
+                              child: Text(
+                                value,
+                                style: const TextStyle(fontSize: 16),
+                              ),
+                            ),
+                          );
+                        }).toList(),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                SizedBox(
+                  width: 150,
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.resolveWith(
+                        (states) => kPrimarycolor,
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>  QRScanner(city: city,gate: gate,),
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      'قراءة الـ QR',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                widget.status == 1
+                    ? Image.asset(
+                        'assets/images/true.png',
+                        width: 140,
+                      )
+                    : widget.status == 2
+                        ? Image.asset(
+                            'assets/images/false.png',
+                            width: 140,
+                          )
+                        : const Text('')
+              ],
+            ),
           ),
         ),
       ),
